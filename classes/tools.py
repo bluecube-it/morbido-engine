2e8e6a098dc07d9b69dbcb74e8807fbb00e31f12
teoobos <matte17spo@gmail.com>
2019-09-13 11:45:26 +0200

    Adding Mape function

--- classes/tools.py
+++ classes/tools.py
@@ -4,3 +4,9 @@ import pandas as pd
 class Tools:
     def describe(self, filename):
         return pd.read_csv(filename).describe().to_json()
+
+    """
+    MAPE: true, actual = float, int, np.array
+    """
+    def mean_absolute_percentage_error(self, true, actual):
+        return np.mean(np.abs((true - actual) / true)) * 100
