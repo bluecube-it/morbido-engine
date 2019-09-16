97ecc0543791ce9d6c6e7b83b14b505f20e05708
teoobos <matte17spo@gmail.com>
2019-09-16 15:22:59 +0200

    iii

--- controllers/prediction.py
+++ controllers/prediction.py
@@ -7,20 +7,18 @@ class Prediction(Resource):
     data:
         filename: string
         seasonality: int
-        precision: int
+        precision: sting
+            - "low": only parsimonia
+            - "medium": log and parsimonia
+            - "high": TODO
         index: string
         input: string
-        prediction: int
+        prediction:
+            - int
+            - string (month)
     """
     def post(self):
         params = request.form['data']
         
         forecasting = Sarima(params['seasonality'], params['precision'])
-        
-        return json.loads(forecasting.get_prediction(params['filename'], [params['index'], params['input']], params['prediction'])
-
-        """
-        usati per la prova
-        prova = Sarima(12, 'low')
-        return json.loads(prova.get_prediction('dataset_finale.csv', ['date', 'values'], 12))
-        """
+        return json.loads(forecasting.get_prediction(params['filename'], [params['index'], params['input']], params['prediction']))
